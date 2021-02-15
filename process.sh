d2d2c8157b3dc89c3aff84115aebeb5b7b1ff32a
Ángel Igareta <32129522+angeligareta@users.noreply.github.com>
2021-02-15 15:25:46 +0100

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Procesador de Saxon 9 XSLT => 3.0
+# Saxon 9 XSLT processor => 3.0
 java -jar lib/saxon9he.jar -a:on -s:src/xml/recipe-book.xml -o:docs/index.html
 mv src/*.html docs/
